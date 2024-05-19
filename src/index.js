846e802a53376c6f2da0ee8496373cf32285a147
Jose Deodoro <deodoro.filho@gmail.com>
2024-05-19 12:47:17 -0400

    both curves (up and down) plotted

--- src/index.js
+++ src/index.js
@@ -1,68 +1,6 @@
-import Plotly from 'plotly.js-dist-min';
 import Phaser from 'phaser';
-import generateContourData from './generateContourData';
-
-// Testing
-async function createEmptyPlotBitmap(value, func) {
-    return new Promise((resolve) => {
-        const canvas = document.createElement('canvas');
-        canvas.width = 800;
-        canvas.height = 600;
-        const ctx = canvas.getContext('2d');
-
-        ctx.fillStyle = 'blue';
-        ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-        resolve(canvas);
-    });
-}
-
-async function createContourPlotBitmap(value, func) {
-    return new Promise((resolve, reject) => {
-        const data = func(value);
-        const layout = {
-            xaxis: {
-                visible: false
-            },
-            yaxis: {
-                visible: false
-            },
-            showlegend: false,
-            annotations: [],
-            title: '',
-            margin: {
-                l: 0,
-                r: 0,
-                t: 0,
-                b: 0
-            },
-            paper_bgcolor: 'rgba(0,0,0,0)',
-            plot_bgcolor: 'rgba(0,0,0,0)'
-        };
-
-        const gd = document.createElement('div');
-        gd.style.backgroundColor = 'rgba(0,0,0,0)';
-        document.body.appendChild(gd);
-        console.dir(data);
-
-        Plotly.newPlot(gd, data, layout).then(function(gd) {
-            Plotly.toImage(gd, {format: 'png', width: 800, height: 600}).then(function(url) {
-                const img = new Image();
-                img.src = url;
-                img.onload = function() {
-                    const canvas = document.createElement('canvas');
-                    canvas.width = img.width;
-                    canvas.height = img.height;
-                    const ctx = canvas.getContext('2d');
-                    ctx.clearRect(0, 0, canvas.width, canvas.height);
-                    ctx.drawImage(img, 0, 0);
-                    document.body.removeChild(gd);
-                    resolve(canvas);
-                };
-            }).catch(reject);
-        }).catch(reject);
-    });
-}
+import { createContourPlotBitmap } from './util/contour';
+import { COBB_DOUGLAS, DYN_HOM_COBB_DOUGLAS, HOM_COBB_DOUGLAS } from './artifacts/utility';
 
 const config = {
     type: Phaser.AUTO,
@@ -77,41 +15,57 @@ const config = {
 
 const game = new Phaser.Game(config);
 
+async function createTexture(u, inverted) {
+    const canvas = await createContourPlotBitmap(u, inverted);
+    return canvas.toDataURL();
+}
+
 async function preload() {
-    const canvas = await createContourPlotBitmap(4, generateContourData);
-    const dataURL = canvas.toDataURL();
-    this.textures.addBase64('contour', dataURL);
+    const dataURL_up = await createTexture(COBB_DOUGLAS([.5,.5]), false);
+    const dataURL_dn = await createTexture(COBB_DOUGLAS([.5,.5]), true);
+    this.textures.addBase64('contour_up', dataURL_up);
+    this.textures.addBase64('contour_dn', dataURL_dn);
 }
 
 async function create() {
-    this.textures.on('onload', (key, texture) => {
-        this.add.image(400, 300, 'contour').setDepth(0);
-        const graphics = this.add.graphics();
+    const textureKeys = ['contour_up', 'contour_dn']; // Add all your texture keys here
+    const loadTexture = (scene, key) => {
+        return new Promise((resolve) => {
+            scene.textures.on('onload', (loadedKey) => {
+                if (loadedKey === key) {
+                    scene.add.image(400, 300, key);
+                    resolve();
+                }
+            });
+        });
+    }
+    const texturePromises = textureKeys.map(key => loadTexture(this, key));
 
-        // Draw a rectangle
+    const initGraphics = () => {
+        const graphics = this.add.graphics();
         rectangle = new Phaser.Geom.Rectangle(0, 300, 100, 50);
         graphics.fillStyle(0x00ff00, 1);
         graphics.fillRectShape(rectangle);
-        this.cameras.main.setBackgroundColor('#808080');
-
-        // Save the graphics object for use in the update loop
+        this.cameras.main.setBackgroundColor('#c0c0c0');
         this.graphics = graphics;
+    }
+    this.textures.on('onload', (key, texture) => {
+        this.add.image(400, 300, key);
     });
+
+    Promise.all(texturePromises).then(() => {
+        initGraphics();
+    });
+
 }
 
 function update() {
     if (this.graphics) {
-        // Clear the previous frame
         this.graphics.clear();
 
-        // Move the rectangle to the right
         rectangle.x += 1;
-
-        // Draw the rectangle at its new position
         this.graphics.fillStyle(0x00ff00, 1);
         this.graphics.fillRectShape(rectangle);
-
-        // Reset the rectangle's position if it goes off screen
         if (rectangle.x > config.width) {
             rectangle.x = -rectangle.width;
         }
---
+++ src/util/contour.js
@@ -0,0 +1,101 @@
+import Plotly from 'plotly.js-dist-min';
+
+const SIZE = 100;
+
+export default function generateContourData(u) {
+    var x = new Array(SIZE),
+        y = new Array(SIZE),
+        z = new Array(SIZE),
+        i,
+        j;
+
+    var d = .01;
+    for (var i = 0; i < SIZE; i++) {
+        x[i] = y[i] = d += .01;
+    }
+
+    x.forEach((v, i) => z[i] = y.map(t => u([v, t])));
+
+    return [{
+        z: z,
+        x: x,
+        y: y,
+        type: "contour",
+        showscale: false,
+        contours: {
+            coloring: 'lines' // Only show lines, no fills
+        },
+        line: {
+            color: 'blue' // Set the contour lines color to blue
+        }
+    }];
+}
+
+async function createEmptyPlotBitmap() {
+    return new Promise((resolve) => {
+        const canvas = document.createElement('canvas');
+        canvas.width = 800;
+        canvas.height = 600;
+        const ctx = canvas.getContext('2d');
+
+        ctx.fillStyle = 'blue';
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+        resolve(canvas);
+    });
+}
+
+async function createContourPlotBitmap(u, inverted = false) {
+    return new Promise((resolve, reject) => {
+        const data = generateContourData(u);
+        const layout = {
+            xaxis: {
+                visible: false
+            },
+            yaxis: {
+                visible: false
+            },
+            showlegend: false,
+            annotations: [],
+            title: '',
+            margin: {
+                l: 0,
+                r: 0,
+                t: 0,
+                b: 0
+            },
+            paper_bgcolor: 'rgba(0,0,0,0)',
+            plot_bgcolor: 'rgba(0,0,0,0)'
+        };
+
+        const gd = document.createElement('div');
+        gd.style.backgroundColor = 'rgba(0,0,0,0)';
+        document.body.appendChild(gd);
+
+        Plotly.newPlot(gd, data, layout).then(function(gd) {
+            Plotly.toImage(gd, {format: 'png', width: 800, height: 600}).then(function(url) {
+                const img = new Image();
+                img.src = url;
+                img.onload = function() {
+                    const canvas = document.createElement('canvas');
+                    canvas.width = img.width;
+                    canvas.height = img.height;
+                    const ctx = canvas.getContext('2d');
+                    ctx.clearRect(0, 0, canvas.width, canvas.height);
+                    if (inverted) {
+                        ctx.translate(canvas.width / 2, canvas.height / 2);
+                        ctx.rotate(Math.PI);
+                        ctx.drawImage(img, -canvas.width/2, -canvas.height/2);
+                    }
+                    else {
+                        ctx.drawImage(img, 0, 0);
+                    }
+                    document.body.removeChild(gd);
+                    resolve(canvas);
+                };
+            }).catch(reject);
+        }).catch(reject);
+    });
+}
+
+export { createEmptyPlotBitmap, createContourPlotBitmap };
