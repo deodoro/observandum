60d362b06f1da139f973e614b2f1b6d091d1d2b7
Jose Deodoro <deodoro.filho@gmail.com>
2024-05-31 00:19:52 -0400

    Takes full windows, readjusts

--- src/index.js
+++ src/index.js
@@ -10,8 +10,8 @@ const DENSITY = 90; // Isoline density
 
 const config = {
     type: Phaser.AUTO,
-    width: 800,
-    height: 600,
+    width: window.innerWidth,
+    height: window.innerHeight,
     scene: {
         preload: preload,
         create: create,
@@ -21,12 +21,17 @@ const config = {
 
 const game = new Phaser.Game(config);
 
+window.addEventListener('resize', () => {
+    game.scale.resize(window.innerWidth, window.innerHeight);
+    adjustElements.call(game.scene.scenes[0]);
+});
+
 async function preload() {}
 
 async function create() {
     const graphics = this.add.graphics();
     this.graphics = graphics;
-    this.cameras.main.setBackgroundColor('#c2b8aa');
+    this.cameras.main.setBackgroundColor('#eee7dd');
 
     // Game state
     const state = {
@@ -40,18 +45,18 @@ async function create() {
     };
     this.game_state = state;
 
-    const circle = new Phaser.Geom.Circle(400, 300, 5);
+    const circle = new Phaser.Geom.Circle(this.cameras.main.centerX, this.cameras.main.centerY, 5);
     this.circle = circle;
 
     this.label_1 = createLabel.call(this, circle.x, circle.y + circle.radius + 10, '#2d4ebb');
     this.label_2 = createLabel.call(this, circle.x, circle.y - circle.radius - 10, '#437e07');
-    this.label_price = createLabel.call(this, 711, 550, '#f0f0f0', 'Spot:', 20);
-    this.label_price_accum = createLabel.call(this, 700, 530, '#f0f0f0', 'Accum:', 20);
+    this.label_price = createLabel.call(this, this.cameras.main.width - 89, this.cameras.main.height - 50, '#f0f0f0', 'Spot:', 20);
+    this.label_price_accum = createLabel.call(this, this.cameras.main.width - 100, this.cameras.main.height - 70, '#f0f0f0', 'Accum:', 20);
 
-    this.button_circle = new Phaser.Geom.Circle(718, 60, 25);
-    const button = createButton.call(this, 700, 50, 'TRADE');
+    this.button_circle = new Phaser.Geom.Circle(this.cameras.main.width - 82, 60, 25);
+    this.button = createButton.call(this, this.cameras.main.width - 100, 50, 'TRADE');
 
-    button.on('pointerdown', () => {
+    this.button.on('pointerdown', () => {
         this.game_state.do_trade = true;
         this.label_price.visible = true;
         this.label_price_accum.visible = true;
@@ -105,15 +110,15 @@ function setEntitlements({ x, y }) {
         ({ individual: new Individual(coords, COBB_DOUGLAS(weights), name, color), translate });
 
     this.game_state.people = [
-        createIndividual([x, y], [0.25, 0.75], 'c1', 0x2d4ebb, a => [a[0] * 800 / DENSITY, (DENSITY - a[1]) * 600 / DENSITY]),
-        createIndividual([1000 - x, 1000 - y], [0.75, 0.25], 'c2', 0x305d04, a => [(DENSITY - a[0]) * 800 / DENSITY, a[1] * 600 / DENSITY])
+        createIndividual([x, y], [0.25, 0.75], 'c1', 0x2d4ebb, a => [a[0] * this.cameras.main.width / DENSITY, (DENSITY - a[1]) * this.cameras.main.height / DENSITY]),
+        createIndividual([1000 - x, 1000 - y], [0.75, 0.25], 'c2', 0x305d04, a => [(DENSITY - a[0]) * this.cameras.main.width / DENSITY, a[1] * this.cameras.main.height / DENSITY])
     ];
 
     updateLabels.call(this);
 }
 
 function setEntitlementsPtr(pointer) {
-    setEntitlements.call(this, { x: Math.trunc(1000 * (pointer.x / 800)), y: 1000 - Math.trunc(1000 * pointer.y / 600) });
+    setEntitlements.call(this, { x: Math.trunc(1000 * (pointer.x / this.cameras.main.width)), y: 1000 - Math.trunc(1000 * pointer.y / this.cameras.main.height) });
 }
 
 function updateLabels() {
@@ -168,8 +173,8 @@ function update() {
 
         const [c1] = this.game_state.people.map(({ individual }) => individual);
         if (c1) {
-            this.circle.x = (c1.getEndowment()[0] * 800) / MAX_ENDOWMENT;
-            this.circle.y = ((MAX_ENDOWMENT - c1.getEndowment()[1]) * 600) / MAX_ENDOWMENT;
+            this.circle.x = (c1.getEndowment()[0] * this.cameras.main.width) / MAX_ENDOWMENT;
+            this.circle.y = ((MAX_ENDOWMENT - c1.getEndowment()[1]) * this.cameras.main.height) / MAX_ENDOWMENT;
             this.label_1.setPosition(this.circle.x, this.circle.y + this.circle.radius + 10);
             this.label_2.setPosition(this.circle.x, this.circle.y - this.circle.radius - 10);
         }
@@ -187,4 +192,17 @@ function update() {
         this.game_state.frame_count++;
         if (this.game_state.frame_count % 10 === 0) this.game_state.on_off = 1 - this.game_state.on_off;
     }
+}
+
+function adjustElements() {
+    const WIDTH = window.innerWidth;
+    const HEIGHT = window.innerHeight;
+
+    this.circle.setPosition(this.cameras.main.centerX, this.cameras.main.centerY);
+    this.label_1.setPosition(this.circle.x, this.circle.y + this.circle.radius + 10);
+    this.label_2.setPosition(this.circle.x, this.circle.y - this.circle.radius - 10);
+    this.label_price.setPosition(WIDTH - 89, HEIGHT - 50);
+    this.label_price_accum.setPosition(WIDTH - 100, HEIGHT - 70);
+    this.button_circle.setPosition(WIDTH - 82, 60);
+    this.button.setPosition(WIDTH - 100, 50);
 }
